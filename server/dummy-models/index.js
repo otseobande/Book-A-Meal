72396ec99231ee5db3458edf63de8764e25f7f12
Otse Obande <otseobande@gmail.com>
2018-04-25 23:31:08 +0100

    [#157011149] Add index.js to dummy-models directory

---
+++ server/dummy-models/index.js
@@ -0,0 +1,14 @@
+import Menus from './menus';
+import Meals from './meals';
+import Orders from './orders';
+import MenuCategories from './menuCategories';
+import MealMenuCategories from './mealMeauCategories';
+
+
+export default {
+	Menus,
+	Meals,
+	Orders,
+	MenuCategories,
+	MealMenuCategories,
+}
