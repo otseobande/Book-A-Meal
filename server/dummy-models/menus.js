ef0011fc4fe3cded31153b440686edcd48860f7e
Otse Obande <otseobande@gmail.com>
2018-04-25 22:23:03 +0100

    [#157011149] Add attribut case to menus dummy model

--- server/dummy-models/menus.js
+++ server/dummy-models/menus.js
@@ -2,20 +2,20 @@ const menus = [
 	{
 		id: 1,
 		userId: 2,
-		title: 'Madolicious',
-		date: "20-10-1980",
+		title: 'Italian cuisine',
+		date: "21-05-2018",
 	},
 	{
 		id: 2,
 		userId: 2,
 		title: 'Chinese menu',
-		date: "20-10-1980",
+		date: "22-05-2018",
 	},
 	{
 		id: 3,
 		userId: 2,
 		title: 'Nigerian menu',
-		date: "20-10-1980",
+		date: "24-05-2018",
 	}
 	
 ];
