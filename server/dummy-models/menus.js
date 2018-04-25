7dc4ac833a5a99f96f4f613c230753918907d55a
Otse Obande <otseobande@gmail.com>
2018-04-25 21:59:27 +0100

    [#157011149] Create menu dummy model

--- server/dummy-models/menus.js
+++ server/dummy-models/menus.js
@@ -1,1 +1,23 @@
- 
+const menus = [
+	{
+		id: 1,
+		userId: 2,
+		title: 'Madolicious',
+		date: "20-10-1980",
+	},
+	{
+		id: 2,
+		userId: 2,
+		title: 'Chinese menu',
+		date: "20-10-1980",
+	},
+	{
+		id: 3,
+		userId: 2,
+		title: 'Nigerian menu',
+		date: "20-10-1980",
+	}
+	
+];
+
+export default menus;
