8ddf2d4bb7e88a8a615ad5b7ec41c5f22cd99305
Otse Obande <otseobande@gmail.com>
2018-04-26 22:30:08 +0100

    [#156852953] TESTS: Write test for route and controller method to get meal for a specific date

--- server/tests/routes/menusRoutes.js
+++ server/tests/routes/menusRoutes.js
@@ -60,3 +60,25 @@ describe('GET /api/v1/menus', () => {
 		}
 	});
 });
+
+describe('GET /api/v1/menus/:date', () => {
+	it('should return status 200', async function() {
+		try {
+			const res = await chai.request(App)
+														.get('/api/v1/menus/05-22-2018');
+			res.should.have.status(200);
+		} catch(err) {
+			throw err;
+		}
+	});
+	it('should return success message', async function() {
+		try {
+			const res = await chai.request(App)
+														.get('/api/v1/menus/05-22-2018');
+			res.body.status.should.be.equal('success');
+			res.body.message.should.be.equal('Date format should be DD-MM-YYYY');
+		} catch(err) {
+			throw err;
+		}
+	})
+});
