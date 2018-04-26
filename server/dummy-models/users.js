bb3cc144c44769c57f52c1771bcb01db1e65e323
Otse Obande <otseobande@gmail.com>
2018-04-26 01:42:14 +0100

    [#157011149] Create dummy notifications test and model

--- server/dummy-models/users.js
+++ server/dummy-models/users.js
@@ -1,23 +1,23 @@
 const users = [
-	{
+  {
     id: 1,
-    name: "Otse Obande",
-    email: "otseobande@gmail.com",
-    password: "bookameal",
+    name: 'Otse Obande',
+    email: 'otseobande@gmail.com',
+    password: 'bookameal',
     role: 'caterer',
   },
   {
     id: 2,
-    name: "John Ade",
-    email: "john@gmail.com",
-    password: "bookameal",
+    name: 'John Ade',
+    email: 'john@gmail.com',
+    password: 'bookameal',
     role: 'customer',
   },
   {
     id: 3,
-    name: "Zappa Balami",
-    email: "zappabalami@gmail.com",
-    password: "bookameal",
+    name: 'Zappa Balami',
+    email: 'zappabalami@gmail.com',
+    password: 'bookameal',
     role: 'caterer',
   },
 ];
