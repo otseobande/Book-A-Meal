319a0904d7abdded2c6dfdc59e166557dd634e43
Otse Obande <otseobande@gmail.com>
2018-04-26 22:21:42 +0100

    [#156852953] Fix: Change date format in menu model

--- server/dummy-models/menus.js
+++ server/dummy-models/menus.js
@@ -3,19 +3,19 @@ const menus = [
     id: 1,
     userId: 2,
     title: 'Italian cuisine',
-    date: '21-05-2018',
+    date: '05-22-2018',
   },
   {
     id: 2,
     userId: 2,
     title: 'Chinese menu',
-    date: '22-05-2018',
+    date: '05-21-2018',
   },
   {
     id: 3,
     userId: 2,
     title: 'Nigerian menu',
-    date: '24-05-2018',
+    date: '05-24-2018',
   },
 
 ];
