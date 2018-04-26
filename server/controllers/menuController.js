f29bf332ee8c846ddea9608b42040f77258b2883
Otse Obande <otseobande@gmail.com>
2018-04-26 18:17:16 +0100

    [#156977386] Ft: Create endpoints for menu retrival with tests

--- server/controllers/menuController.js
+++ server/controllers/menuController.js
@@ -83,6 +83,47 @@ class MenuController extends Controller {
   		data: responseData,
   	})
   }
+
+  static getTodaysMenu(req, res){
+		const responseData = [];  	
+  	Menus.some(menu => {
+  		const data = {
+  			id: menu.id,
+  			title: menu.title,
+  			date: menu.date,
+  		};
+  		const categories = [];
+  		MenuCategories.forEach(category => {
+  			if(parseInt(category.menuId, 10) === parseInt(menu.id, 10)){
+  				const categoryData = {
+  					id: category.menuId,
+  					title: category.title,
+  				}
+
+  				const meals = [];
+  				MealMenuCategories.forEach(mealMenu => {
+  					Meals.forEach(meal => {
+  						if(parseInt(meal.id, 10) === parseInt(mealMenu.mealId, 10) &&
+  							parseInt(category.id, 10)  === parseInt(mealMenu.menuCategoryId, 10)){
+  							meals.push(meal);
+  						}
+  					});
+  				});
+
+  				categoryData.meals = meals;
+  				categories.push(categoryData);
+  			}
+  		});
+  		data.categories = categories;
+  		responseData.push(data);
+  		return parseInt(menu.id, 10) === 1;
+  	});
+
+  	res.status(200).json({
+  		status: 'success',
+  		data: responseData,
+  	})
+  }
 }
 
 export default MenuController;
