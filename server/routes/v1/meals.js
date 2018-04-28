e70336b465b6357d13c7edffa80f78d33fe51208
Otse Obande <otseobande@gmail.com>
2018-04-28 10:33:07 +0100

    [#156853178] Fix: Restructured routes

--- server/routes/v1/meals.js
+++ server/routes/v1/meals.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import MealController from '../controllers/mealController';
+import MealController from '../../controllers/mealController';
 
 const router = express.Router();
 
-// These routes are to be prepended with "/meals"
 router.get('/', MealController.getMeals);
 router.post('/', MealController.createMeal);
 router.get('/:mealId', MealController.getMeal);
--- server/routes/v1/orders.js
+++ server/routes/v1/orders.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import OrderController from '../controllers/orderController';
+import OrderController from '../../controllers/orderController';
 
 const router = express.Router();
 
-// These routes are to be prepended with "/orders"
 router.get('/', OrderController.getAllOrders);
 router.post('/', OrderController.createOrder);
-
 
 export default router;
