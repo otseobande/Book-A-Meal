db15eb652d0b5ff12f223f9ef821966cd815d12c
Otse Obande <otseobande@gmail.com>
2018-04-29 00:41:47 +0100

    [#156853178] Test: Write update menu route test

---
+++ server/tests/routes/MenuRoutes.js/testUpdateMenuRoute.js
@@ -0,0 +1,55 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import App from "../../../app";
+
+chai.use(chaiHttp);
+chai.should();
+
+
+describe('PUT /api/v1/menus/:date', function() {
+    it('should return a success status 202', async function() {
+        try {
+            const res = await chai.request(App)
+                .put('/api/v1/menus/05-22-2018')
+                .send({
+                    title: 'test menu',
+                    categories: [{
+                            title: "Benue style",
+                            mealIds: [1, 3]
+                        },
+                        {
+                            title: "Jacuzzi paruzi",
+                            mealIds: [2, 3]
+                        }
+                    ]
+                });
+
+            res.should.have.status(202);
+        } catch (err) {
+            throw err;
+        }
+    });
+
+    it('should return an error 404 if not found', async function() {
+        try {
+            const res = await chai.request(App)
+                .put('/api/v1/menus/04-10-2018')
+                .send({
+                    title: 'test menu',
+                    categories: [{
+                            title: "Benue style",
+                            mealIds: [1, 3]
+                        },
+                        {
+                            title: "Jacuzzi paruzi",
+                            mealIds: [2, 3]
+                        }
+                    ]
+                });
+
+            res.should.have.status(404);
+        } catch (err) {
+            throw err;
+        }
+    });
+});
