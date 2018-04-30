fedfc9a016d10c0c08de917f40da407f347a1be9
Otse Obande <otseobande@gmail.com>
2018-04-30 22:56:11 +0100

    [#157203061] converted orderController to a class

---
+++ server/controllers/orderController.js
@@ -0,0 +1,75 @@
+import orders from '../dummy-models/orders';
+import users from '../dummy-models/users';
+import meals from '../dummy-models/meals';
+
+class OrderController{
+  static createOrder(req, res) {
+    const {
+      mealId,
+      quantity,
+      deliveryAddress
+    } = req.body;
+
+    if (!mealId || !quantity || !deliveryAddress) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Parameters supplied incorrectly'
+      });
+    }
+
+    orders.create({
+      userId: 2,
+      mealId,
+      quantity,
+      deliveryAddress
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Order created successfully'
+    });
+  };
+
+  static getAllOrders(req, res) {
+    const responseData = [];
+    orders.data.forEach((order) => {
+      const linkedUser = users.find(user => parseInt(user.id, 10) === parseInt(order.userId, 10));
+      const linkedMeal = meals.find(meal => parseInt(meal.id, 10) === parseInt(order.mealId, 10));
+      responseData.push({
+        id: order.id,
+        quantity: order.quantity,
+        deliveryAddress: order.deliveryAddress,
+        user: linkedUser,
+        meal: linkedMeal
+      });
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      data: responseData
+    });
+  };
+
+  static updateOrder(req, res) {
+    const { orderId } = req.params;
+
+    const updatedOrder = orders.update(
+      req.body,
+      order => parseInt(order.id, 10) === parseInt(orderId, 10)
+    );
+
+    if (Object.keys(updatedOrder).length > 0) {
+      return res.status(202).json({
+        status: 'success',
+        message: 'order updated successfully'
+      });
+    }
+
+    return res.status(404).json({
+      status: 'error',
+      message: 'order not found'
+    });
+  };
+}
+
+export default OrderController;
