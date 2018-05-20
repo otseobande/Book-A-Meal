78cbba3986c2df6be767253007f558e6643486ef
Otse Obande <otseobande@gmail.com>
2018-05-20 21:53:08 +0100

    Update testEnsureDateIsValid to check if next is called

--- server/tests/middlewares/testEnsureDateIsValid.js
+++ server/tests/middlewares/testEnsureDateIsValid.js
@@ -8,9 +8,15 @@ import { validateCreate } from '../../middlewares/validators/menu';
 
 const [ensureDateIsValid] = validateCreate;
 
-const req = mockReq({
+const badReq = mockReq({
 	body: {
 		date: 'asdfadfa'
+	}
+});
+
+const goodReq = mockReq({
+	body: {
+		date: '2018-04-01'
 	}
 });
 
@@ -20,12 +26,18 @@ describe('ensureDateIsValid middleware', () => {
 	const next = sinon.spy();
 	
 	it('should return error message if date is invalid', () => {
-		ensureDateIsValid(req, res, next);
+		ensureDateIsValid(badReq, res, next);
 
 		res.status.should.have.been.calledWith(400);
 		res.json.should.have.been.calledWith({
       status: 'error',
       message: 'Date is invalid'
     })
+	})
+
+	it('should call next if date is valid', () => {
+		ensureDateIsValid(goodReq, res, next);
+		
+		next.should.have.been.called;
 	})
 })
