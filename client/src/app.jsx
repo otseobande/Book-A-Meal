9d518806090f0fdbd91b81d2e991df7287ceb0ec
Otse Obande <otseobande@gmail.com>
2018-06-06 22:28:36 +0100

    [#157903353] Configure react router

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 
 render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root') // eslint-disable-line no-undef
 );
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
 import Home from './Home/index.jsx';
 
-const App = () => <Home />;
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+  </Switch>
+);
 
 export default App;
