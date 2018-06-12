2a1e84eb76b85a962dc28a18f90f6311b9acf989
Otse Obande <otseobande@gmail.com>
2018-06-12 07:52:39 +0100

    Require ignore-styles for mocha

--- client/tests/components/layout/Footer.test.js
+++ client/tests/components/layout/Footer.test.js
@@ -3,7 +3,7 @@ import chai from 'chai';
 import enzyme, { render } from 'enzyme';
 import chaiJestSnapshot from 'chai-jest-snapshot';
 import Adapter from 'enzyme-adapter-react-16';
-import Footer from '../../../src/components/layout/Footer.jsx';
+import Footer from '../../../src/components/shared/Footer/index.jsx';
 
 enzyme.configure({ adapter: new Adapter() });
 chai.use(chaiJestSnapshot);
